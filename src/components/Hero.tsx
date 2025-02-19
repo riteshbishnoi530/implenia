38e90fcf741e875a10c8c7b8aa15a83596c9bd90
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2025-02-19 16:35:58 +0530

    add image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import CustomBtn from './common/CustomBtn'
 import Header from './common/Header'
+import Image from 'next/image'
 
 function Hero() {
   return (
     <div className='bg-white'>
       <Header />
-      <div className='lg:pl-[50px] bg-hero bg-cover bg-no-repeat bg-center lg:h-[662px] max-lg:py-20 flex flex-col justify-center px-4'>
-        <div className="max-w-[1138px] mx-auto w-full">
+      <div className='lg:pl-[50px] relative bg-hero bg-cover bg-no-repeat bg-center lg:h-[662px] max-lg:py-20 flex flex-col justify-center px-4'>
+        <Image className='absolute bottom-0 left-0 max-lg:max-w-[200px] max-md:max-w-[150px]' src='/assets/images/Boxly-winking-left.webp' alt='logo' width={400} height={400}/>
+        <div className="relative z-[1] max-w-[1138px] mx-auto w-full">
           <p className='text-white text-custom-sm max-md:text-base uppercase font-bold drop-shadow-[0px_4px_4px_0px_#00000040]'>Shape the future of Implenia!</p>
           <h1 className='text-white text-custom-lg leading-custom-sm max-lg:text-6xl max-md:text-5xl max-sm:text-4xl font-bold max-w-[800px] max-lg:max-w-[600px] max-md:max-w-[500px] max-sm:max-w-[400px] mb-[25px]'>WE ARE LOOKING FOR YOUR IDEA</h1>
           <CustomBtn btn='Bring your idea to life!' />
