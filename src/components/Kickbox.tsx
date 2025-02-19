31247656062ef10ff8c1c27a5d6c0b55c7dc0078
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2025-02-19 21:48:18 +0530

    counter

--- src/components/Kickbox.tsx
+++ src/components/Kickbox.tsx
@@ -1,10 +1,12 @@
+"use client"
 import React from 'react'
 import CustomHeading from './common/CustomHeading'
 import { KICK_BOX_LIST } from '@/utils/helper'
 import CustomBtn from './common/CustomBtn'
+import CountUp from 'react-countup'
 
 function Kickbox() {
-    
+
     return (
         <div className='bg-white pt-[134px] max-lg:py-20 max-md:py-16 max-sm:py-10 pb-[129px] px-4'>
             <div className='max-w-[1200px] mx-auto'>
@@ -14,7 +16,10 @@ function Kickbox() {
                     <div className='flex max-[850.98px]:flex-wrap max-[850.98px]:gap-10 justify-center items-center mt-5'>
                         {KICK_BOX_LIST.map((obj, i) => (
                             <div key={i} className={`${i === 0 ? "min-[850.98px]:mr-[94px]" : ""} ${i === 1 ? "min-[850.98px]:mr-[97px]" : ""}`}>
-                                <h2 className='text-custom-xl max-lg:text-6xl max-md:text-5xl max-md:text-center text-yellow-400 leading-custom-md'>{obj.number}</h2>
+                                <h2 className='text-custom-xl max-lg:text-6xl max-md:text-5xl max-md:text-center text-yellow-400 leading-custom-md'>
+                                    +  <CountUp start={0} end={
+                                        i === 0 ? 155 : i === 1 ? 325 : i === 2 ? 250 : 0} duration={5} />
+                                </h2>
                                 <p className='text-custom-sm font-bold text-black max-sm:text-center'>{obj.ideas}</p>
                             </div>
 
